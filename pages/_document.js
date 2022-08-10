fd35cccef65ff3aaeed03ff01660b54ebe843edf
Keng Hong <kenghong95@gmail.com>
2022-08-10 11:38:53 +0800

    update Gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import { ColorModeScript } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
+
+import { GA_TRACKING_ID } from '../lib/gtag'
 import theme from '../lib/theme'
 
 export default class Document extends NextDocument {
@@ -7,6 +9,20 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GA_TRACKING_ID}', {
+page_path: window.location.pathname,
+});
+`
+          }}
+        />
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
